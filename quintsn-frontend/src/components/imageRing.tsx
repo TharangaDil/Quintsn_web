eea57d40c2ad5499ef59931e32022031137d2023
TharangaDil <147192645+TharangaDil@users.noreply.github.com>
2025-12-08 18:57:22 +0530

    Change the images

--- quintsn-frontend/src/components/imageRing.tsx
+++ quintsn-frontend/src/components/imageRing.tsx
@@ -5,13 +5,13 @@ import ThreeDImageRing from "./ui/imageRing";
 
 export default function ImageRing() {
   const items = [
-    { url: "/img/img1.png", label: "Services", href: "/serviceDetails" },
-    { url: "/img/img2.png", label: "Blog", href: "/blog" },
-    { url: "/img/img3.png", label: "SAP Business One", href: "/servicesDetails" },
-    { url: "/img/img4.png", label: "SAP by Design", href: "/businessConsultant" },
-    { url: "/img/img5.png", label: "Microsoft Dynamic 365", href: "/" },
-    { url: "/img/img6.png", label: "SAGE ERP 300", href: "/blog" },
-    { url: "/img/img8.png", label: "Oracle Netsuit", href: "/servicesDetails" },
+    { url: "https://images.pexels.com/photos/1704120/pexels-photo-1704120.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "Services", href: "/serviceDetails" },
+    { url: "https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "Blog", href: "/blog" },
+    { url: "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "SAP Business One", href: "/servicesDetails" },
+    { url: "https://images.pexels.com/photos/912110/pexels-photo-912110.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "SAP by Design", href: "/businessConsultant" },
+    { url: "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "Microsoft Dynamic 365", href: "/" },
+    { url: "https://images.pexels.com/photos/114979/pexels-photo-114979.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "SAGE ERP 300", href: "/blog" },
+    { url: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1200", label: "Oracle Netsuit", href: "/servicesDetails" },
     
   ];
 
